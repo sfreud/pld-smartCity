814d5916aae312678d963cb4144b64b55e802a1d
yannvuadel <yann.vuadel@gmail.com>
2015-04-29 08:44:02 +0200

    Modification MainActivity
    Modification MainActivity

--- AndroidClient/app/src/main/java/com/example/androidclient/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/androidclient/MainActivity.java
@@ -1,19 +1,73 @@
 package com.example.androidclient;
-
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
+    Button b1 = null;
+    TextView t1 = null;
+    TextView t2 =  null;
+    TextView t3 =  null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        b1 = (Button) findViewById(R.id.button);
+        t1 = (TextView) findViewById(R.id.textView);
+        t2 = (TextView) findViewById(R.id.textView2);
+        t3 = (TextView) findViewById(R.id.textView3);
+
+        b1.setOnClickListener( new View.OnClickListener() {
+                                   @Override
+                                   public void onClick(View v) {
+
+                                       ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+                                       NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                                       if(networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected()) {
+                                           boolean wifi = networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
+                                           t1.setText("NetworkState " + " L'interface de connexion active est du Wifi : " + wifi);
+                                       }
+
+                                       if(networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected()) {
+                                           boolean mobile = networkInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+                                           t2.setText("NetworkState " + " L'interface de connexion active est du mobile : " + mobile);
+                                       }
+
+                                       /*SendingGetRequestTask sg = new SendingGetRequestTask();
+                                       sg.execute();*/
+                                   }
+                               }
+        );
+        SendingGetRequestTask sg = new SendingGetRequestTask();
+        sg.execute();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +90,87 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    protected class SendingGetRequestTask extends AsyncTask<TextView, Void, String> {
+        String contentAsString = "";
+
+        @Override
+        protected String doInBackground(TextView... params) {
+            InputStream inputStream = null;
+            String result = "";
+            try {
+
+                // create HttpClient
+                HttpClient httpclient = new DefaultHttpClient();
+                String url = "http://10.0.2.2:8182"/*"http://www.android.com"*/;
+                // make GET request to the given URL
+                HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+                // receive response as inputStream
+                inputStream = httpResponse.getEntity().getContent();
+
+                // convert inputstream to string
+                if(inputStream != null)
+                    result = getStringFromInputStream(inputStream);
+                else
+                    result = "Did not work!";
+
+            } catch (Exception e) {
+                Log.d("InputStream", e.getLocalizedMessage());
+            }
+            contentAsString = result;
+            Log.d("ENDING", "Requete get terminee");
+            Log.d("Page Web",contentAsString);
+        /*try {
+
+            URL url = new URL("http://www.android.com");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            contentAsString = getStringFromInputStream(in);
+            urlConnection.disconnect();
+            Log.d("ENDING", "Requete get terminee");
+            Log.d("Page Web",contentAsString);
+        } catch (Exception e) {
+            contentAsString = e.toString();
+            e.printStackTrace();
+        }*/
+
+            return contentAsString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            t3.setText(contentAsString);
+        }
+
+        // convert InputStream to String
+        private String getStringFromInputStream(InputStream is) {
+
+            BufferedReader br = null;
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            try {
+
+                br = new BufferedReader(new InputStreamReader(is));
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return sb.toString();
+        }
+    };
+
 }

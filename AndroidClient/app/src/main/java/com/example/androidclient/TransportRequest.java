e7bae007f22685e96639306b31e22a9e43b10e20
yannvuadel <yann.vuadel@gmail.com>
2015-04-29 11:57:27 +0200

    Ajout Transport Request

---
+++ AndroidClient/app/src/main/java/com/example/androidclient/TransportRequest.java
@@ -0,0 +1,67 @@
+package com.example.androidclient;
+
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Event;
+
+public class TransportRequest {
+
+
+
+    /**
+     * arrivalAddress: adresse du lieu de l'évènement
+     * startAddress: adresse du lieu de départ
+     * startStop : adresse de l'arrêt de départ
+     * arrivalStop : adresse de l'arrêt d'arrivée
+     * beginEvent : date de début de l'évènement
+     * */
+    private String arrivalAddress;
+    private String startAddress;
+    private String startStop;
+    private String arrivalStop;
+    private DateTime beginEvent;
+
+
+
+    public TransportRequest(Event event, String startAddress)
+    {
+        this.startAddress = startAddress;
+        this.beginEvent = event.getStart().getDateTime();
+        this.arrivalAddress = event.getLocation();
+        this.startStop = null;
+        this.arrivalStop = null;
+
+    }
+
+    public String getArrivalStop() {
+        return arrivalStop;
+    }
+
+    public void setArrivalStop(String arrivalStop) {
+        this.arrivalStop = arrivalStop;
+    }
+
+    public String getStartStop() {
+        return startStop;
+    }
+
+    public void setStartStop(String startStop) {
+        this.startStop = startStop;
+    }
+    public String getArrivalAddress() {
+        return arrivalAddress;
+    }
+    public String getStartAddress() {
+        return startAddress;
+    }
+
+    public DateTime getBeginEvent() {
+        return beginEvent;
+    }
+
+    public Object calculateTransport()
+    {
+        return null;
+    }
+
+
+}

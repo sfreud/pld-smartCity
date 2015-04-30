31e0a1246bda0e595fe57f8b11a3b0feb8a0a120
stroux <stephane.roux@insa-lyon.fr>
2015-04-30 11:19:03 +0200

    Dikjstra fin graphe

--- AndroidClient/app/src/main/java/com/example/dijkstra/Graph.java
+++ AndroidClient/app/src/main/java/com/example/dijkstra/Graph.java
@@ -3,17 +3,165 @@ package com.example.dijkstra;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Graph {
+public class Graph {
 
-    protected List<GraphNode> graphNodes;
-    protected List<GraphEdge> graphEdges;
-
+    private GraphNode begin;
     public Graph() {
-        graphNodes = new ArrayList<GraphNode>();
-        graphEdges = new ArrayList<GraphEdge>();
+        begin = new GraphNode(0);
     }
 
-    public abstract List<GraphNode> getGraphNodes();
+    public List<GraphNode> getGraphNodes(){
+        List<GraphNode> greyNodes = new ArrayList<>();
+        List<GraphNode> blackNodes = new ArrayList<>();
+        List<GraphNode> ret = new ArrayList<>();
+        greyNodes.add(begin);
+        ret.add(begin);
+        while(!greyNodes.isEmpty()){
+            GraphNode cur = greyNodes.get(0);
+            List<GraphNode> sons = cur.getSons();
+            for(GraphNode n : sons){
+                if(!(greyNodes.contains(n))&&!(blackNodes.contains(n)))
+                {
+                    greyNodes.add(n);
+                    ret.add(n);
+                }
+            }
+            greyNodes.remove(cur);
+            blackNodes.add(cur);
+        }
+        
+        return ret;
+    }
+    
+    public long getBiggestNumberNode()
+    {
+        long max = begin.getID();
+        List<GraphNode> greyNodes = new ArrayList<>();
+        List<GraphNode> blackNodes = new ArrayList<>();
+        greyNodes.add(begin);
+        while(!greyNodes.isEmpty()){
+            GraphNode cur = greyNodes.get(0);
+            List<GraphNode> sons = cur.getSons();
+            for(GraphNode n : sons){
+                if(!(greyNodes.contains(n))&&!(blackNodes.contains(n)))
+                {
+                    greyNodes.add(n);
+                    if(n.getID()>max)
+                    {
+                        max=n.getID();
+                    }
+                }
+            }
+            greyNodes.remove(cur);
+            blackNodes.add(cur);
+        }
+        return max;
+    }
+    
+    public boolean addEdge(long nbBegin, long nbEnd, long length)
+    {
+        GraphNode nodeBegin = null;
+        GraphNode nodeEnd = null;
+        List<GraphNode> greyNodes = new ArrayList<>();
+        List<GraphNode> blackNodes = new ArrayList<>();
+        greyNodes.add(begin);
+        if(begin.getID()==nbBegin)
+        {
+            nodeBegin=begin;
+        }
+        if(begin.getID()==nbEnd)
+        {
+            nodeEnd=begin;
+        }    
+        while(!greyNodes.isEmpty()){
+            GraphNode cur = greyNodes.get(0);
+            List<GraphNode> sons = cur.getSons();
+            for(GraphNode n : sons){
+                if(!(greyNodes.contains(n))&&!(blackNodes.contains(n)))
+                {
+                    greyNodes.add(n);
+                    if(n.getID()==nbBegin)
+                    {
+                        nodeBegin=n;
+                    }
+                    if(n.getID()==nbEnd)
+                    {
+                        nodeEnd=n;
+                    }
+                    
+                }
+            }
+            greyNodes.remove(cur);
+            blackNodes.add(cur);
+        }
+        
+        if(nodeBegin == null)
+        {
+            return false;
+        }
+        if(nodeEnd == null)
+        {
+            nodeEnd = new GraphNode(nbEnd);
+        }
+        new GraphEdge(nodeBegin,nodeEnd,length);
+        return true;
+        
+        
+    }
+    
+    public void displayGraph()
+    {
+        List<GraphNode> listNodes = this.getGraphNodes();
+        for(GraphNode dep : listNodes)
+        {
+            List<GraphEdge> listEdge = dep.getEdgeLeaving();
+            for(GraphEdge leav : listEdge)
+            {
+                System.out.println(dep.getID().toString()+" ->("+leav.getWeight().toString()+") "+leav.getEnd().getID().toString());
 
-    public abstract List<GraphEdge> getGraphEdges();
+            }
+        }
+    }
+    
+    public Pair<List<GraphNode>, Long> getShorterWay(GraphNode begin, GraphNode end)
+    {
+        return DijkstraUnit.dijkstraAlgorithm(begin, end, this);
+    }
+    
+    public GraphNode getNode(long ID)
+    {
+        GraphNode ret = null;
+        List<GraphNode> greyNodes = new ArrayList<>();
+        List<GraphNode> blackNodes = new ArrayList<>();
+        greyNodes.add(begin);
+        if(begin.getID()==ID)
+        {
+            ret=begin;
+        }
+        else
+        {
+            while(!greyNodes.isEmpty()){
+                GraphNode cur = greyNodes.get(0);
+                List<GraphNode> sons = cur.getSons();
+                for(GraphNode n : sons){
+                    if(!(greyNodes.contains(n))&&!(blackNodes.contains(n)))
+                    {
+                        greyNodes.add(n);
+                        if(n.getID()==ID)
+                        {
+                            ret=n;
+                        }
+                        
+
+                    }
+                }
+                greyNodes.remove(cur);
+                blackNodes.add(cur);
+            }
+        }
+        return ret;
+        
+        
+    }
+    
 }
---
+++ AndroidClient/app/src/main/java/com/example/dijkstra/test.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.dijkstra;
+
+import java.util.List;
+
+/**
+ *
+ * @author Stéphane ROUX
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Graph g = new Graph();
+        g.addEdge(0, 1, 5);
+        g.addEdge(0, 2, 3);
+        g.addEdge(0, 3, 2);
+        g.addEdge(1, 5, 2);
+        g.addEdge(1, 4, 2);
+        g.addEdge(2, 1, 1);
+        g.addEdge(3, 2, 1);
+        g.addEdge(3, 4, 3);
+        g.addEdge(3, 6, 2);
+        g.addEdge(5, 4, 4);
+        g.addEdge(5, 7, 7);
+        g.addEdge(6, 5, 1);
+        g.addEdge(6, 7, 6);
+        g.displayGraph();
+        
+        //Sol : 0,3,6,7
+        
+        //E : 0, A : 1, B : 2, C : 3, G : 4, D : 5, F : 6, S : 7
+        List<GraphNode> way = g.getShorterWay(g.getNode(0), g.getNode(7)).first;
+        for(GraphNode n : way)
+        {
+            System.out.print(n.getID()+"->");
+        }
+        System.out.println();
+    }
+}

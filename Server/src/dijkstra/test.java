d1ffaff7c3bd93b6ade6256ea01917c910109577
stroux <stephane.roux@insa-lyon.fr>
2015-05-03 13:42:28 +0200

    test

--- Server/src/dijkstra/test.java
+++ Server/src/dijkstra/test.java
@@ -4,10 +4,14 @@
  */
 package dijkstra;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.xml.sax.*;
+import org.w3c.dom.*;
 /**
  *
  * @author Stéphane ROUX
@@ -18,5 +22,8 @@ public class test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        }
+        
+
+     }
 }
+
